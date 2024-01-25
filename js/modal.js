87da16d7ed94a626d05ed8b827779f8f23971e95
Oleksandr Yatsenko <iosiguides@gmail.com>
2024-01-25 15:51:01 +0200

    Added optimize for mobile && Bugs fixed

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,10 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modal = document.querySelector(".backdrop");
+const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalBtnClose = document.querySelector(".modal-btn-close");
+const mobileModalBtnOpen = document.querySelector('.mobile-modal-btn-open');
+const toggleModal = () => modal.classList.toggle("is-hidden");
 
-const toggleModal = () => {
-    modal.classList.toggle('is-hidden');
-};
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnClose.addEventListener("click", toggleModal);
+mobileModalBtnOpen.addEventListener('click', toggleModal);
